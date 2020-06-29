e878fd1c49b5dce960173482ecf4b5b5e8e58f5c
QasimKhanUtmanzai <32437076+QasimKhanUtmanzai@users.noreply.github.com>
2020-06-29 19:11:39 +0200

    Add files via upload

--- prime.py
+++ prime.py
@@ -1,1 +1,11 @@
+n=int(input("Enter any number = "))
+if(n==1):
+        print(n,"is not a Prime Number")
+else:
+    for i in range(2, n):
 
+        if n % i == 0:
+            print(n, "is not a Prime Number")
+            break
+    else:
+        print(n, "is a prime number")
